46e791e32039f7136ce474542507cac1af3c2b76
KazuhaKun <zou123@outlook.com>
2025-02-25 21:12:53 +0800

    Complete TEST4

--- TEST4/main.c
+++ TEST4/main.c
@@ -5,26 +5,27 @@
 #include "Key.h"
 #include "Timer0.h"
 //#include "AT24C02.h"
-#include "Buzzer.h"
+// #include "Buzzer.h"
 #include "nixie.h"
 #include "XPT2046.h"
 
 
 
-// sbit Light_Green_A = P2^0;
-// sbit Light_Yellow_A = P2^1;
-// sbit Light_Red_A = P2^2;
-// sbit Light_Green_B = P2^4;
-// sbit Light_Yellow_B = P2^6;
-// sbit Light_Red_B = P2^7;
-sbit Light_Green_A = P1^0;
-sbit Light_Yellow_A = P1^1;
-sbit Light_Red_A = P1^2;
-sbit Light_Green_B = P1^3;
+sbit Light_Green_A = P2^0;
+sbit Light_Yellow_A = P2^1;
+sbit Light_Red_A = P2^5;
+sbit Light_Green_B = P2^6;
 sbit Light_Yellow_B = P1^4;
-sbit Light_Red_B = P1^5;
+sbit Light_Red_B = P2^7;
 
-sbit Buzzer = P2^5;
+// sbit Light_Green_A = P1^0;
+// sbit Light_Yellow_A = P1^1;
+// sbit Light_Red_A = P1^2;
+// sbit Light_Green_B = P1^3;
+// sbit Light_Yellow_B = P1^4;
+// sbit Light_Red_B = P1^5;
+
+// sbit Buzzer = P2^5;
 
 unsigned char status;
 unsigned char FlashFlag_Light_Yellow = 0;
@@ -42,8 +43,8 @@ void Nixie_CountDown(unsigned char num)
 	j = num%10;
 	// Nixie(1,i);
 	// Nixie(2,j);
-	Nixie_SetBuf(1,i);
-	Nixie_SetBuf(2,j);
+	Nixie_SetBuf(7,i);
+	Nixie_SetBuf(8,j);
 }
 
 void main()
@@ -73,7 +74,7 @@ void main()
 				Light_Yellow_A = FlashFlag_Light_Yellow;
 				//Road B:Do nothing
 				//Buzzer
-				if(FlashFlag_Light_Yellow) Buzzer_Time(100);
+				// if(FlashFlag_Light_Yellow) Buzzer_Time(100);
 				//CountDown
 				Nixie_CountDown(CountDown);
 				break;
@@ -97,7 +98,7 @@ void main()
 				Light_Yellow_B = FlashFlag_Light_Yellow;
 				//Road A:Do nothing
 				//Buzzer
-				if(FlashFlag_Light_Yellow) Buzzer_Time(100);
+				// if(FlashFlag_Light_Yellow) Buzzer_Time(100);
 				//CountDown
 				Nixie_CountDown(CountDown);
 				break;
@@ -111,6 +112,9 @@ void main()
 				Light_Green_B = 1;
 				Light_Yellow_B = 1;
 				Light_Red_B = 0;
+				//Nixie Clear
+				Nixie_SetBuf(7,10);
+				Nixie_SetBuf(8,10);
 				break;
 			}
 			case 5:		//Night Mode
@@ -122,6 +126,9 @@ void main()
 				Light_Green_B = 1;
 				Light_Red_B = 1;
 				Light_Yellow_B = FlashFlag_Light_Yellow;
+				//Nixie Clear
+				Nixie_SetBuf(7,10);
+				Nixie_SetBuf(8,10);
 				break;
 			}
 		}
@@ -131,28 +138,47 @@ void main()
 
 void Timer0_Routine() interrupt 1
 {
-	static unsigned int KeyCount, LightCount, FlashCount, T0Count;
+	static unsigned int KeyCount, LightCount, FlashCount, T0Count, GRCount;
+	static bit NightMode = 0;
 	TL0 = 0x66; 
 	TH0 = 0xFC;
 	KeyCount++;
 	LightCount++;
 	FlashCount++;
 	T0Count++;
+	GRCount++;
 	//Nixie Loop
 	if(T0Count >= 2){T0Count = 0;Nixie_Loop();}
-	//GR Scan
-	if(T0Count >= 2){T0Count = 0;ADValue=XPT2046_ReadAD(XPT2046_VBAT);if(ADValue < 190) status = 5;if(ADValue > 160) status = 0;}
+	//Night Mode
+	if(GRCount >= 100){
+		GRCount = 0;
+		ADValue = XPT2046_ReadAD(XPT2046_VBAT);
+		if(ADValue < 50) {NightMode = 1;}
+		else {NightMode = 0;}
+
+		if(NightMode && status != 5) {
+            status = 5;
+            LightCount = 0;  // 重置LightCount
+        } else if(!NightMode && status == 5) {
+            status = 0;
+            LightCount = 0;  // 重置LightCount
+        }
+	}
 	//Key Detect
 	if(KeyCount >= 20){KeyCount = 0;Key_Loop();}
 	//Flash Flag
 	if (FlashCount >= 500){FlashCount = 0;FlashFlag_Light_Yellow = ~FlashFlag_Light_Yellow;}
+	// if ((status == 1 || status == 3 || status == 5) && LightCount % 500 == 1) {FlashFlag_Buzzer = ~FlashFlag_Buzzer;}
 	// //Buzzer Control
 	// if(FlashCount >= 500 && (status == 1 || status == 3)) {FlashCount = 0;FlashFlag_Buzzer = ~FlashFlag_Buzzer;}
 	//Light Control
-	if(status == 0 && LightCount >= 5000){LightCount = 0;status = 1;}
-	if(status == 1 && LightCount >= 2000){LightCount = 0;status = 2;}
-	if(status == 2 && LightCount >= 5000){LightCount = 0;status = 3;}
-	if(status == 3 && LightCount >= 2000){LightCount = 0;status = 0;}
+	if(!NightMode){	
+		if(status == 0 && LightCount >= 5000){LightCount = 0;status = 1;}
+		if(status == 1 && LightCount >= 2000){LightCount = 0;status = 2;}
+		if(status == 2 && LightCount >= 5000){LightCount = 0;status = 3;}
+		if(status == 3 && LightCount >= 2000){LightCount = 0;status = 0;}
+	}
+	//Emergency
 	if(status == 4 && LightCount >= 10000){LightCount = 0;status = 0;}
 	//LED Num
 	if(LightCount%1000 == 1) {
